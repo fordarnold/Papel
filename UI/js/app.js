99773d0f38c029f34e358398b288582b7eb3b4cb
Robin Hood <fordarnold@gmail.com>
2020-02-13 14:45:43 +0200

    feat (accounts): Test account deletion simulation

--- UI/js/app.js
+++ UI/js/app.js
@@ -63,3 +63,26 @@ function redirectUserSession(userType) {
 }
 
 
+
+/**
+ * https://www.sitepoint.com/javascript-event-delegation-is-easier-than-you-think
+ */
+
+// function getEventTarget(e) {
+// 	e = e || window.event;
+// 	return e.target || e.srcElement;
+// }
+  
+// function deleteAccountRow(e) {
+
+// 	// confirm that the target element is a table cell by checking its tag name
+// 	var target = getEventTarget(e);
+
+// 	if(target.tagName.toLowerCase() === 'td') {
+// 		alert(target);
+// 	}
+// }
+
+// document.getElementById("table-accounts").addEventListener("click", function (event) { 
+// 	deleteAccountRow(event); 
+// });
