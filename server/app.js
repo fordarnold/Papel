6ae8e384187b9ecb331ba0cc302f1ca9f74a5747
Robin Hood <fordarnold@gmail.com>
2020-02-18 15:22:29 +0200

    chore (app): setup Express framework
    - Configured express, nodemon and dotenv lib modules successfully.
    - Also completed successful setup of our first (base) endpoint.
    
    [Finishes #171272249]

--- server/app.js
+++ server/app.js
@@ -1,4 +1,32 @@
-// import express from "express"; // Express web framework
+import 'dotenv/config';
+import express from 'express'; // Express web framework
+
+const app = express(); // Create the Express app server
+const PORT = process.env.PORT || 3000; // Specify the server port
+
+/**
+ * COMMON API ROUTES & END-POINTS
+ * -------------------------------------------------------------------
+ */
+
+// Default (landing) route
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'Papel API Server welcomes you!',
+  });
+});
+
+/**
+ * START SERVER
+ * -------------------------------------------------------------------
+ */
+
+// start the server, on specified port
+const server = app.listen(PORT, () => console.log(`Papel API server listening at: http://localhost:${PORT}!`));
+
+export default server;
+
 // import bodyParser from "body-parser"; // Handle HTTP POST requests with 'req.body' in Express framework (~https://stackoverflow.com/questions/38306569/what-does-body-parser-do-with-express)
 
 // import routes from "./routes/routes.v1"; // Import API routes (v1)
