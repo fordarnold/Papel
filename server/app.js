8bd9b623d64d341d956fa0e6f64eb4f1f33f65e1
Robin Hood <fordarnold@gmail.com>
2020-02-19 15:27:02 +0200

    chore (authentication): setup POST request handler
    In Express JS, we require a middleware service to extract the body of an HTTP POST request.

--- server/app.js
+++ server/app.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 3000; // Specify the server port
 
 app.use(cors());
 
+// Handle HTTP POST requests with 'req.body' in Express framework ~https://stackoverflow.com/a/43626891/2661028
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 /**
  * COMMON API ROUTES & END-POINTS
  * -------------------------------------------------------------------
