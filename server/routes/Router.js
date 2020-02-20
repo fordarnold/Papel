e12a49fc0389f283e9406a3d3d9ce864b84918cb
Robin Hood <fordarnold@gmail.com>
2020-02-20 09:59:10 +0200

    chore (app): setup a router file
    To manage REST API routes.

---
+++ server/routes/Router.js
@@ -0,0 +1,16 @@
+import Router from 'express';
+
+import UserController from '../controllers/UserController';
+
+const router = Router();
+
+// // Sample routes...
+// router.post('/auth/signup', (req, res) => {
+//   res.send('hello world');
+// });
+
+
+// Auth routes
+router.post('/auth/signup', UserController.register);
+
+export default router;
