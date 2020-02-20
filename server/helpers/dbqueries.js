5422493c4efdaeeca8862a64de6b671f16e7ff48
Robin Hood <fordarnold@gmail.com>
2020-02-20 09:50:33 +0200

    feat (users): add SQL queries for Users entity
    For interacting with the 'users' table in the database

--- server/helpers/dbqueries.js
+++ server/helpers/dbqueries.js
@@ -3,11 +3,11 @@ const getUsers = 'SELECT * FROM users';
 const createUsersTable = `CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(50) NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NULL,
+    last_name VARCHAR(50) NULL,
     password VARCHAR(70) NOT NULL,
     type VARCHAR(10) NOT NULL,
-    is_admin VARCHAR(10) NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
     created_on TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL)`;
 
 const createUserRecord = `INSERT INTO users (email, first_name, last_name, password, type, is_admin) 
